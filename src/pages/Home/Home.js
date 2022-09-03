de7395ab5ee7e4ecf672a1a79aa99c169b8a1ee2
Anthony Parrino <anthonyparrino@Anthonys-Air.attlocal.net>
2022-09-03 01:40:24 -0400

    working on header

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './home.css';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div>
+            <div className='header-container'>
+                <h1 className='tracking-in-expand-fwd-top'>Anthony Parrino</h1>
+                <br />
+                <p className='focus-in-contract'>Full Stack Software Engineer</p>
+                <br />
+                <div className='skills-container'>
+                    <p className="text-focus-in">REACT JS</p>
+                    <p style={{ animation: 'text-focus-in 1.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both' }} className="text-focus-in">JAVASCRIPT</p>
+                    <p style={{ animation: 'text-focus-in 2s cubic-bezier(0.550, 0.085, 0.680, 0.530) both' }} className="text-focus-in">NODE</p>
+                    <p style={{ animation: 'text-focus-in 2.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) both' }} className="text-focus-in">MONGODB</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Home
